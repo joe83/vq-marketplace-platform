448acad0cffad94bea16146da518045b9ba5966b
Adrian Barwicki <adrianbarwicki@gmail.com>
2017-10-29 12:43:17 +0100

    fix in subdomain tenant detection

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
 
 	console.log(`Accessing ${subdomains}`);
 
-	const tenantId = subdomains[0];
+	const tenantId = subdomains[subdomains.length - 1];
 
 	req.models = db.get(tenantId);
 
