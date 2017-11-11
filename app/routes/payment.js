9bd0a266b60b4848b9da96014e340779ca883a3b
Adrian Barwicki <adrianbarwicki@gmail.com>
2017-11-11 17:21:45 +0100

    added payment route for creating stripe accounts

--- app/routes/payment.js
+++ app/routes/payment.js
@@ -1,8 +1,49 @@
-const stripe = require("stripe")("sk_test_Hops88AqZwLgedHPbzyiTXps");
+const stripe = require("stripe")("sk_test_YBNHjAXJ2I1fKOrQaLTdP0e4");
 const responseController = require("../controllers/responseController.js");
 const isLoggedIn = responseController.isLoggedIn;
+const isAdmin = responseController.isAdmin;
+
+const tenantDb = require("../../app-tenant/models");
+
+const createAccount = (type, country, email, cb) => {
+    stripe.accounts.create({
+        type,
+        country,
+        email,
+   })
+   .then(rAccount => {
+       console.log(rAccount);
+
+       cb(null, rAccount);
+   }, err => {
+       console.log(err);
+
+       cb(err);
+   });
+};
+
+// createAccount("standard", "hu", "adrianbarwicki+2@gmail.com");
 
 module.exports = app => {
+    app.post("/api/payment/:paymentProvider/account", isLoggedIn, isAdmin, (req, res) => {
+        const tenantModels = tenantDb.get();
+
+        tenantModels
+        .tenant
+        .findOne({
+            tenantId: req.models.tenantId
+        })
+        .then(rTenant => {
+            createAccount("standard", rTenant.country, rTenant.email, (err, rAccount) => {
+                if (err) {
+                    return res.status(400).send(err);
+                }
+
+                return res.send(rAccount);
+            });
+        });
+    });
+
     app.post("/api/payment/sub", isLoggedIn, (req, res) => {
         var customer = {};
         var User = req.user;
