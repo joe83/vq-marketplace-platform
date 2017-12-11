076ce3135b02e33af21618ec536aa51fc36b7b78
Adrian Barwicki <adrianbarwicki@gmail.com>
2017-12-11 19:34:39 +0100

    hotfix, increased message limits

--- app/models/chat.js
+++ app/models/chat.js
@@ -1,7 +1,7 @@
 module.exports = (sequelize, DataTypes) => {
   const Message = sequelize.define("message", {
       message: {
-        type: DataTypes.STRING, allowNull: false
+        type: DataTypes.TEXT, allowNull: false
       }
   }, {
       tableName: "message"
