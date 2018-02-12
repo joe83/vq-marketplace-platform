9f4084f59ea0f0f000acdd6341a27b5097eaea87
Adrian Barwicki <adrianbarwicki@gmail.com>
2018-02-12 21:40:54 +0100

    cleanup deployment scripts

--- migrations/last-migration.sql
+++ migrations/last-migration.sql
@@ -6,6 +6,11 @@ create procedure doMigration ()
 		declare continue handler for 1060 begin end;
 		ALTER TABLE task ADD COLUMN callToActionLabel VARCHAR(64);
         ALTER TABLE task ADD COLUMN callToActionUrl VARCHAR(254);
+		ALTER TABLE tenant ADD COLUMN chargebeeCustomerId VARCHAR(255);
+        ALTER TABLE tenant ADD COLUMN chargebeeActiveSubscriptionId VARCHAR(255);
+		ALTER TABLE request MODIFY COLUMN status ENUM('0','5','6','10','14','15','20','25');
+		ALTER TABLE _appTaskCategory ADD COLUMN status ENUM('0', '103', '99');
+		ALTER TABLE task MODIFY COLUMN status ENUM('0','10','20','30','99','103');
 	end;;
     
 call doMigration();
