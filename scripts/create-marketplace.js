4b406dd49e955f0455409a21090c3d7b439ea2a3
Sercan AKMAN <sercan@tdsmaker.com>
2018-02-23 02:26:32 +0300

    test for create-marketplace script

---
+++ scripts/create-marketplace.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+const db = require('../built/app/models/models.js');
+
+const TENANT_ID = process.argv[3] || process.env.TENANT_ID;
+const USECASE = process.argv[2];
+
+if (!USECASE) {
+    throw new Error('Specify USECASE');
+}
+
+db.create(TENANT_ID, USECASE, () => {
+    console.log('Database created for ' + USECASE + ' marketplace for tenant ' + TENANT_ID);
+    db.get(TENANT_ID)
+        .appConfig
+        .addDefaultConfig(USECASE, (err, data) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log('Configs restored for ' + USECASE + ' marketplace for tenant ' + TENANT_ID);
+        });
+
+    db.get(TENANT_ID)
+        .appLabel
+        .addDefaultLangLabels(TARGET_LANG, USECASE, SHOULD_FORCE, (err, data) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log('Labels restored for ' + USECASE + ' marketplace for tenant ' + TENANT_ID);
+        });
+    db.get(TENANT_ID)
+        .post
+        .addDefaultPosts(USECASE, true, (err, data) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log('Labels restored for ' + USECASE + ' marketplace for tenant ' + TENANT_ID);
+        });  
+});
