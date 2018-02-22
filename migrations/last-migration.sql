466bc275483bb80b4f7e83b947562fa0695311e4
Adrian Barwicki <adrianbarwicki@gmail.com>
2018-02-22 13:51:27 -0800

    update of emails

--- migrations/last-migration.sql
+++ migrations/last-migration.sql
@@ -6,8 +6,10 @@ create procedure doMigration ()
 		declare continue handler for 1060 begin end;
 		ALTER TABLE _posts MODIFY COLUMN eventTrigger ENUM('order-closed','new-order','order-completed','order-marked-as-done');
 	
-		UPDATE _posts SET targetUserType = 2 WHERE code = 'request-marked-as-done';
-		UPDATE _posts SET eventTrigger = 'order-marked-as-done' WHERE code='request-marked-as-done';
+		UPDATE _posts SET targetUserType = 1 WHERE code = 'new-order';
+		UPDATE _posts SET eventTrigger = 'new-order' WHERE code='new-order';
 
+		UPDATE _posts SET targetUserType = 2 WHERE code = 'new-order-for-supply';
+		UPDATE _posts SET eventTrigger = 'new-order' WHERE code='new-order-for-supply';
 	end;;
 call doMigration();
