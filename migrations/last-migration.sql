fb2355d5e02aa72d69f263be4a560b2bf0359d64
Adrian Barwicki <adrianbarwicki@gmail.com>
2018-03-15 12:07:20 -0700

    fix unitOfMeasure

--- migrations/last-migration.sql
+++ migrations/last-migration.sql
@@ -4,12 +4,6 @@ delimiter ;;
 create procedure doMigration ()
 	begin
 		declare continue handler for 1060 begin end;
-		ALTER TABLE _posts MODIFY COLUMN eventTrigger ENUM('order-closed','new-order','order-completed','order-marked-as-done');
-	
-		UPDATE _posts SET targetUserType = 1 WHERE code = 'new-order';
-		UPDATE _posts SET eventTrigger = 'new-order' WHERE code='new-order';
-
-		UPDATE _posts SET targetUserType = 2 WHERE code = 'new-order-for-supply';
-		UPDATE _posts SET eventTrigger = 'new-order' WHERE code='new-order-for-supply';
+		ALTER TABLE task ADD COLUMN unitOfMeasure VARCHAR(10);
 	end;;
 call doMigration();
